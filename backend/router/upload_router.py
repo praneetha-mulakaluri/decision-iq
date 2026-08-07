3f4e3bad0886ae01924afd939ed38fe3ed556d2e
Praneetha <praneetha.sai159@gmail.com>
2026-08-07 16:55:44 +0530

    cleaning repo

--- backend/router/upload_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, UploadFile, File
-from services.upload_service import process_csv
-
-router = APIRouter(prefix="/csv", tags=["CSV"])
-
-@router.post("/upload")
-async def upload_csv(file: UploadFile = File(...)):
-    return await process_csv(file)
